40fa15ce463f42e3b5e26878732ddecd9b5b89b4
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-01-03 15:55:22 +0100

    Correction route DELETE

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
-
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
